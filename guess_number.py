93b81cf57b2c13baebceb46668fe23d87a010cc6
Md Farukuzzaman <farukuzzaman.bd@gmail.com>
2024-03-12 17:44:03 -0500

    Added logic for too high or too low guessed number.

--- guess_number.py
+++ guess_number.py
@@ -3,11 +3,24 @@ number = 10
 while True:
     print("I'm thinking of a number...")
     guess = input("What number am I thinking of? ")
-    if guess == str(number):
-        print("Congratulations! You guessed the right number.")
-        break
-    elif guess == 'q':
+    upper_bound = 15
+    lower_bound = 5
+
+    if guess == 'q':
         print(f"The correct number was {number}.")
         break
     else:
-        print(f"Sorry! Try again.")
+        try:
+            num = int(guess)
+            if num == number:
+                print("Congratulations! You guessed the right number.")
+                break
+            else:
+                if num > upper_bound:
+                    print("Sorry! the number is too high.")
+                elif num < lower_bound:
+                    print("Sorry! The number is too low.") 
+                else:
+                    print("Sorry! Try again.")
+        except:
+            print("Enter a number or q.")
