988d1905d02a81f69009ec7c6d61d5cd2ca9ebdf
Md Farukuzzaman <farukuzzaman.bd@gmail.com>
2024-03-12 18:04:44 -0500

    Added limit to attempt the guessed number, and showing how many guessed left.

--- guess_number.py
+++ guess_number.py
@@ -1,13 +1,29 @@
 number = 10
+limit = 5
+
+print(f"You can try {limit} times to guess a number.")
 
 while True:
     print("I'm thinking of a number...")
     guess = input("What number am I thinking of? ")
-    if guess == str(number):
-        print("Congratulations! You guessed the right number.")
+
+    limit = limit - 1
+
+    if (limit <= 0):
+        print("You have reached your limit to attempt for a guessed number.")
         break
-    elif guess == 'q':
+
+    if guess == 'q':
         print(f"The correct number was {number}.")
         break
     else:
-        print(f"Sorry! Try again.")
+        try:
+            num = int(guess)
+            if num == number:
+                print("Congratulations! You guessed the right number.")
+                break
+            else:
+                print("Sorry! Try again.")
+                print(f"You have {limit} attempt(s) left.")
+        except:
+            print("Enter a number or q.")
